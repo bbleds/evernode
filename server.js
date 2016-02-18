1d2217b79b7c72f2968af433c86ecd13bd5b44e6
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-02-18 10:52:53 -0600

    finished feature: connected to mongodb and added ability to save a new note

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
+const MONGODB_URL = "mongodb://localhost:27017/evernode"
 
 // init app
 const app = express();
+
+const Note = mongoose.model('Notes', mongoose.Schema({
+	title : String,
+	text : String
+}));
 
 //configure body parser
 app.use(bodyParser.urlencoded(
@@ -32,11 +38,25 @@ app.get("/notes/new", (req, res) =>
 //route for making new notes
 app.post("/notes", (req, res) =>
 {
-	console.log(req.body);
-	res.redirect("/")
+	Note.create(req.body, (err, note) =>
+	{
+		if (err) throw err
+		console.log(note);
+		res.redirect("/")
+	})
 })
 
-app.listen(PORT, () =>
+//connect to mongo
+mongoose.connect(MONGODB_URL);
+
+//when mongo is connected
+mongoose.connection.on("open", () =>
 {
-	console.log(`app listening on port: ${PORT}`);
+	//Start app on specified port
+	app.listen(PORT, () =>
+	{
+		console.log(`port running on port ${PORT}`);
+		console.log(`Connected to mongodb`);
+	})
+
 })
