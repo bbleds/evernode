f95a722b9d9a0999bdcfe08fda641aa94894375f
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-02-18 10:35:10 -0600

    added views directory, and set up form submit

--- server.js
+++ server.js
@@ -9,9 +9,31 @@ const PORT = process.env.PORT || 3000;
 // init app
 const app = express();
 
+//configure body parser
+app.use(bodyParser.urlencoded(
+	{
+		extended: false
+	}));
+
+// use jade as view engine
+app.set("view engine", "jade");
+
 app.get("/", (req, res) =>
 {
 	res.send("<h1>Welcome to mah pagggggeeee</h1>");
+})
+
+//route for getting create note form
+app.get("/notes/new", (req, res) =>
+{
+	res.render("new-note");
+})
+
+//route for making new notes
+app.post("/notes", (req, res) =>
+{
+	console.log(req.body);
+	res.redirect("/")
 })
 
 app.listen(PORT, () =>
